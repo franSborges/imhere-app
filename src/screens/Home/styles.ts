59c876e787cfe7d6263db10eec640c19379a0de0
franSborges <borgesfran988@gmail.com>
2023-05-03 20:31:58 -0300

    :feat: first commit

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 30,
+  },
+  eventName: {
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    fontSize: 16,
+    color: "#6b6b6b",
+    fontWeight: "bold",
+    marginTop: 5,
+  },
+});
